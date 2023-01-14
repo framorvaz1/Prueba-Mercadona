d67f9a352ca19d6c9af13a6c7f43a60065ac3fcc
Fran Moreno <francisco.moreno@babelgroup.com>
2023-01-14 12:20:02 +0100

    First commit: App Config

---
+++ Prueba Mercadona/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba Mercadona
+//
+//  Created by francisco.moreno.local on 14/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
